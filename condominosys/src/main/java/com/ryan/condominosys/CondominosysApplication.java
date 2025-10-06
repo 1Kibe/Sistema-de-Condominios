0a49e1b09f913f7180c8eb71369404a46b3dd586
1Kibe <ryanadrianamendoin@email.com>
2025-10-06 12:22:06 -0300

    first commit

---
+++ condominosys/src/main/java/com/ryan/condominosys/CondominosysApplication.java
@@ -0,0 +1,13 @@
+package com.ryan.condominosys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CondominosysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CondominosysApplication.class, args);
+	}
+
+}
---
+++ condominosys/src/test/java/com/ryan/condominosys/CondominosysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryan.condominosys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CondominosysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
